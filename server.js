a62fc0ca1985b8dbb66a025ed536d67e6f30aaee
shivamashtikar <lightningshivam@gmail.com>
2017-08-16 20:03:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.get('/', function (req, res) {
 app.get('/article-one', function(req,res){
     res.send("this is from article one and all the contents will be displayed here");
 });
+app.get('/article-two', function(req,res){
+    res.send("this is from article two and all the contents will be displayed here");
+});
+app.get('/article-three', function(req,res){
+    res.send("this is from article three and all the contents will be displayed here");
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
