dba6439c8090d8ccf794f305613d3cd1ab0c273d
shivamashtikar <lightningshivam@gmail.com>
2017-08-17 13:09:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,11 @@ var element = document.getElementById('main-text');
 element.innerText= "new value";
 //move the  image
 var img =document.getElementById('madi');
+var marginLeft =0;
+function moveRight(){
+    marginLeft= marginLeft + 10;
+    img.style.marginLeft=marginLeft+ 'px';
+}
 img.onclick =function (){
-    img.style.marginLeft= '100px';
+    var interval = setInterval(moveRight,100);
 }; 
