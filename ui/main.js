cdfdc00ba5c012b3b8e6f2c286b7c83c17496f52
shivamashtikar <lightningshivam@gmail.com>
2017-08-19 13:33:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,13 +32,31 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function (){
   //make a request  and send name
+   //  make a request
+  var request = new XMLHttpRequest();
+  //capture the response and store it in a variable
+  request.onreadystatechange = function ()  {
+      if(request.readyState == XMLHttpRequest.DONE){
+          //Take some action
+            if(request.status==200){
+                  var names =request.responseText; 
+                  names =JSON.parse(names); 
+                  var list = '';
+                  for (var i=0; i<names.length;i++){
+                      list += '<li>'+ names[i] +'</li>';
+                  }
+                  var ul = document.getElementById('namelist');
+                  ul.innerHTMl = list;
+                  }
+                        
+      }
+      //not done yet
+      
+  };
+  //Make the request
+  request.open('GET','http://lightningshivam.imad.hasura-app.io/submit-name?name='+ name,true);
+  request.send(null);
   //capture a list of names and render it as list
-  var names =['name1','name2','name3','name4'];
-  var list = '';
-  for (var i=0; i<names.length;i++){
-      list += '<li>'+ names[i] +'</li>';
-  }
-    var ul = document.getElementById('namelist');
-    ul.innerHTMl = 'shivam';
+  
     
 };
