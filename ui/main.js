fd8f8638832e8e595c97a9c34952c9a7cd2b9457
shivamashtikar <lightningshivam@gmail.com>
2017-08-19 12:52:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,3 +26,19 @@ button.onclick = function (){
   
   
 }; 
+//submit name
+var nameInput = document.getElementById("name");
+var nameval = nameInput.value;
+var submit = document.getElementById("submit_btn");
+submit.onclick = function (){
+  //make a request  and send name
+  //capture a list of names and render it as list
+  var names =["name1","name2","name3"];
+  var list = '';
+  for (var i=0; i<names.length;i++){
+      list += '<li>'+ names[i] +'</li>';
+  }
+    var ul = document.getElementById('namelist');
+    ul.innerHTMl = list;
+    
+};
